857469ee2a1918a8e82e1fb125ea7e3eaba9faae
Kevin San Gabriel <ksangabr@sfu.ca>
2021-10-03 21:36:14 -0700

    Updated home component with introduction

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,11 @@ import './Home.css';
 function Home(props) {
   return (
     <div className="Home">
-      sup
+      <section className="introduction">
+        <h1>Kevin San Gabriel</h1>
+        <h3>Front-end developer</h3>
+        <p>I'm a front-end developer based in Langley BC, Canada</p>
+      </section>
     </div>
     
   );
