7b1e05f75d6d9ff45118640060145f7c7d5c6d9f
Kevin San Gabriel <ksangabr@sfu.ca>
2021-11-07 22:18:11 -0800

    Added social list icons

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,14 @@
 import './Navbar.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons' 
+
+library.add(faGithub); 
+library.add(faTwitter);
+library.add(faLinkedin);
+
 
 class Navbar extends React.Component {
 
@@ -15,6 +23,7 @@ class Navbar extends React.Component {
     }));
   }
   render() {
+
     const { isNavVisible } = this.state;
     return(
       <div className="Navbar">
@@ -29,6 +38,22 @@ class Navbar extends React.Component {
           </div>
         </div>
         <nav id="navigation" className={isNavVisible ? "shown" : "hidden"}>
+          <section id="Navbar-bio">
+            <img id="Navbar-bio-photo" src="./images/bio_photo_placeholder2.jpeg" height="200" width="200" alt="profile_picture"></img>
+            <p id="Navbar-bio-description">Hi, my name is Kevin San Gabriel and I'm a junior front-end web developer.  Welcome to my personal website!</p>
+            <ul id="Navbar-social-list">
+              <li className="Navbar-social-item">
+                <FontAwesomeIcon icon={['fab', 'github']} size="2x" color="white"/>
+              </li>
+              <li className="Navbar-social-item">
+                <FontAwesomeIcon icon={['fab', 'twitter']} size="2x" color="white"/>
+              </li>
+              <li className="Navbar-social-item">
+                <FontAwesomeIcon icon={['fab', 'linkedin']} size="2x" color="white"/>
+              </li>
+            </ul>
+          </section>
+
           <ul>
             <li>
               <Link to="/">Home</Link>
