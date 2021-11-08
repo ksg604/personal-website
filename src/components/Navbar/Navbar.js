496f042000149ab785ec2ad1a485d7d8c8c28331
Kevin San Gabriel <ksangabr@sfu.ca>
2021-11-07 16:14:07 -0800

    Added hiding navbar logic to mobile navbar button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,49 @@
 import './Navbar.css';
-import { Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Navbar(props) {
-  return(
-    <div className="Navbar">
+class Navbar extends React.Component {
 
-      <div id="Navbar-button">
-        <div className="Navbar-button-line"></div>
-        <div className="Navbar-button-line"></div>
-        <div className="Navbar-button-line"></div>
+  constructor(props) {
+    super(props);
+    this.state = {isNavVisible: false};
+    this.navigationRef = React.createRef();
+    this.toggleNavigation = this.toggleNavigation.bind(this);
+  }
+  toggleNavigation() {
+    this.setState(prevState => ({isNavVisible: 
+      !prevState.isNavVisible
+    }));
+  }
+  render() {
+    const { isNavVisible } = this.state;
+    return(
+
+      
+      <div className="Navbar">
+  
+        <div id="Navbar-button" onClick={this.toggleNavigation}>
+          <div className="Navbar-button-line"></div>
+          <div className="Navbar-button-line"></div>
+          <div className="Navbar-button-line"></div>
+        </div>
+        <nav id="navigation" className={isNavVisible ? "" : "hidden"} ref={this.navigationRef}>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>   
       </div>
-      <nav className="hidden">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>   
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default Navbar;
