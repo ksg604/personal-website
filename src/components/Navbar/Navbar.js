f0a0b95d819e5860cd0ea73a2d1deaca94a91dff
Kevin San Gabriel <ksangabr@sfu.ca>
2021-11-14 22:53:53 -0800

    Added dark mode toggle button

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons' 
-import {faUser, faLaptopCode, faFileAlt, faEnvelopeOpenText, faPaperPlane} from '@fortawesome/free-solid-svg-icons';
+import {faUser, faLaptopCode, faFileAlt, faEnvelopeOpenText, faPaperPlane, faAdjust} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faGithub, faLaptopCode, faTwitter, faLinkedin, faUser, faFileAlt, faEnvelopeOpenText, faPaperPlane); 
+library.add(faGithub, faLaptopCode, faTwitter, faLinkedin, faUser, faFileAlt, faEnvelopeOpenText, faPaperPlane, faAdjust); 
 
 class Navbar extends React.Component {
 
@@ -32,13 +32,13 @@ class Navbar extends React.Component {
             <div className="Navbar-button-line"></div>
           </div>
           <div id="Navbar-name-container">
-            <h2 id="Navbar-name">Kevin San Gabriel</h2>
+            <h2 id="Navbar-name" class="white-text">Kevin San Gabriel</h2>
           </div>
         </div>
         <nav id="navigation" className={isNavVisible ? "shown" : "hidden"}>
           <section id="Navbar-bio">
             <img id="Navbar-bio-photo" src="./images/bio_photo_placeholder2.jpeg" height="200" width="200" alt="profile_picture"></img>
-            <p id="Navbar-bio-description">Hi, my name is Kevin San Gabriel and I'm a junior front-end web developer.  Welcome to my personal website!</p>
+            <p id="Navbar-bio-description" class="white-text">Hi, my name is Kevin San Gabriel and I'm a junior front-end web developer.  Welcome to my personal website!</p>
             <ul id="Navbar-social-list">
               <li className="Navbar-social-item">
                 <FontAwesomeIcon icon={['fab', 'github']} size="2x" color="white"/>
@@ -58,7 +58,7 @@ class Navbar extends React.Component {
                 <Link to="/about"> 
                   <FontAwesomeIcon icon={['fas', 'user']} size="2x" color="white" fixedWidth className="nav-link-icon"/>
                   <div>
-                    <h3>About Me</h3>
+                    <h3 className="white-text">About Me</h3>
                   </div>
                 </Link>
               </li>
@@ -66,7 +66,7 @@ class Navbar extends React.Component {
                 <Link to="/portfolio">
                   <FontAwesomeIcon icon={['fas', 'laptop-code']} size="2x" color="white" fixedWidth className="nav-link-icon"/>
                   <div>
-                    <h3>Portfolio</h3>
+                    <h3 className="white-text">Portfolio</h3>
                   </div>
                 </Link>
               </li>
@@ -74,7 +74,7 @@ class Navbar extends React.Component {
                 <Link to="/resume">
                   <FontAwesomeIcon icon={['fas', 'file-alt']} size="2x" color="white" fixedWidth className="nav-link-icon"/>
                   <div>
-                    <h3>Resume</h3>
+                    <h3 className="white-text">Resume</h3>
                   </div>
                 </Link> 
               </li>
@@ -82,24 +82,32 @@ class Navbar extends React.Component {
                 <Link to="/contact">
                   <FontAwesomeIcon icon={['fas', 'envelope-open-text']} size="2x" color="white" fixedWidth className="nav-link-icon"/>
                   <div>
-                    <h3>Contact</h3>
+                    <h3 className="white-text">Contact</h3>
                   </div>
                 </Link> 
               </li>
             </ul>
             <div id="hire-me-button">
               <Link to="/contact">
-                <div>
-                  <div id="hire-me-container">
-                    <FontAwesomeIcon icon={['fas', 'paper-plane']} size="lg" color="white"/>
-                    <div>
-                      <h3>Hire Me</h3>
-                    </div>
+                <div id="hire-me-container">
+                  <FontAwesomeIcon icon={['fas', 'paper-plane']} size="lg" color="white"/>
+                  <div>
+                    <h3 className="white-text">Hire Me</h3>
                   </div>
                 </div>
-                
               </Link>
             </div>
+          </section>
+          <hr></hr>
+          <section id="Navbar-dark-mode-container">
+            <div>
+              <FontAwesomeIcon icon={['fas', 'adjust']} size="lg" color="white"/>
+              <h4 id="dark-mode-title" className="white-text">
+                Dark Mode
+              </h4>
+            </div>
+            <input id="dark-mode-toggle"  type="checkbox"></input>
+            <label id="dark-mode-toggle-label" for="dark-mode-toggle"></label>
           </section>
         </nav>   
       </div>
